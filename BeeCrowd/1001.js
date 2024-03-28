d0939b005489d33ade61878f0334a51a1adc4b6d
Wellington M Braganca <braganca.wellington@icloud.com>
2024-03-28 17:00:25 -0400

    std

--- BeeCrowd/1001.js
+++
@@ -1,9 +0,0 @@
-var input = require('fs').readFileSync('stdin', 'utf8')
-
-var valoresRecebidos = input.split('\n')
-
-var A = parseInt(valoresRecebidos.shift())
-var B = parseInt(valoresRecebidos.shift())
-var X = A + B
-
-console.log(X);
